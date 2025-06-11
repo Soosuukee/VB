f7555bac645c2f783cd35fb18316ae87fa3a1d80
Soosuukee <pro.manimbelflo@gmail.com>
2025-06-11 10:51:11 +0200

    update connexion a la bdd , fuck pgadmin

--- internal/database/db.go
+++ internal/database/db.go
@@ -19,15 +19,14 @@ func Connect() {
 		log.Println("⚠️  .env non trouvé, je continue avec les variables système.")
 	}
 
-	host := os.Getenv("POSTGRES_HOST")
 	user := os.Getenv("POSTGRES_USER")
 	password := os.Getenv("POSTGRES_PASSWORD")
 	dbname := os.Getenv("POSTGRES_DB")
 	port := "5432"
 
 	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		host, user, password, dbname, port,
+		"user=%s password=%s dbname=%s port=%s sslmode=disable",
+		user, password, dbname, port,
 	)
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
